a4967ccc6750b78979a81bcf814dff60ddff08f5
jimmyis <jimmy.israkhan@gmail.com>
2018-02-16 16:07:45 +0700

    ADDED description comments to routes

--- backend/router.js
+++ backend/router.js
@@ -5,6 +5,10 @@ module.exports = function (db) {
   
   return new Router()
     .get('/', (ctx) => ctx.body = `Nothing's here`)
+
+    // This route will create a todo list
     .post('/todo', ctx => controllers.create(ctx))
+
+    // This route will send back all todo lists
     .get('/todo', async ctx => await controllers.listAll(ctx))
 } 
